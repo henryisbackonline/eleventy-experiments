c9a2975b1cacc7b70def777c90f958c08f350252
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-12-09 20:21:58 +1000

    test webp build time from scratch: 1.70 sec

--- eleventy.config.js
+++ eleventy.config.js
@@ -88,7 +88,7 @@ export default async function(eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// Output formats for each image.
 		// One highly compatible format (like WebP) improves build time dramatically.
-		formats: ["png"],
+		formats: ["webp"],
 
 		// Fixed smaller widths for each image imporve build time dramatically.
 		widths: [1200],
